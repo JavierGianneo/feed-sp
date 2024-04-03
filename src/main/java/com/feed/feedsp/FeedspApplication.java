c328dd06f07cfb201847f8ae8f39034e70849926
Lenovo <javier.gianneo@gmail.com>
2024-04-03 13:41:54 -0300

    Starting

---
+++ src/main/java/com/feed/feedsp/FeedspApplication.java
@@ -0,0 +1,13 @@
+package com.feed.feedsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feed/feedsp/FeedspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feed.feedsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
